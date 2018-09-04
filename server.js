b97bb888bdaaca6157527975208189df97c2e6a4
LordJax <40278484+LordJax@users.noreply.github.com>
2018-09-03 21:26:44 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 app.use(express.static('src'))
 
 app.get("/", (request, response) => {
-  response.sendFile('appindex.html')
+  response.sendFile('./appindex.html')
 })
 
 const listener = app.listen(process.env.PORT, () => {
